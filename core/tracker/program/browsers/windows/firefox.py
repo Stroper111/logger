54c9fe67c1d96dca3f79b48ad2a70d95da018fdd
Thymen Rijpkema <t.d.j.rijpkema@gmail.com>
2020-04-23 14:48:13 +0200

    Added unie code to firefox patterns

--- core/tracker/program/browsers/windows/firefox.py
+++ core/tracker/program/browsers/windows/firefox.py
@@ -59,6 +59,7 @@ class Firefox(BaseProgram):
                     "Y\d Q\d - .* - Online LaTeX Editor Overleaf": 'unie',
                     "[\d\w]{6} - .* - Online LaTeX Editor Overleaf": 'unie',
                     "https://www.youtube.com/watch.*": 'Youtube',
+                    ".*[\d\w]{5} \(\d{4}-GS\d\)": 'unie',
                     }
         for key, value in patterns.items():
             if len(re.findall(key, window_name)) > 0:
